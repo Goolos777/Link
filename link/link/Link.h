b53b17607b650fcbd63c6e4951e2d4b2a08e639b
Goolos777 <Goolos777@gmail.com>
2014-01-13 22:30:43 +0200

    update Link.h

---
+++ link/link/Link.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <iostream>
+#include <ctime>
+#include <Windows.h>
+using namespace std;
+
+class Link
+{
+	int length;
+	Link *link;
+public:
+	Link();
+	~Link();
+	
+	unsigned int GetLength(){ return length; };// возвращает длину массива
+
+	void Push(int value);//добавление элемента в конец
+	void Shift(int value);//(добавление элемента в начало 
+
+	void Pop();// удаление  последний
+	void Unshift();// удаление первого элемента
+	bool Remove(int index);//удаление элемента из массива по указанному индексу
+	bool Remove(Link &);//удаление элемента из массива 
+
+	void Insert(int index, int value);// вставка 
+	void Clear();//очистка массива
+	void Reverse();
+
+	void Shuffle();// (случайное перемешивание элементов массива)
+	void Sort();//сортировка массива
+	void Print()const;
+
+	const Link* GetElem(int index)const;
+	void SetElem(int, Link*);
+	
+	void Sort();//организовать not work 
+	bool findLink();
+
+	bool operator==(Link &l)const;
+	int& operator[](unsigned int index)const;
+	friend std::ostream &operator<<(std::ostream&, Link&);
+};
